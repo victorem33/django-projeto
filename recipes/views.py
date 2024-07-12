663035c78dd3f72cfb3b1ad714a4e6e9be6f223b
victor <victor.em.1789@gmail.com>
2024-07-12 19:54:26 -0300

    introduzindo arquivos e construção de códigos reaproveitáveis

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'victor'
     })
 
