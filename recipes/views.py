276f89d2a3c4459507ca0df5bb2ec2e11fc6a340
victor <victor.em.1789@gmail.com>
2024-07-12 17:04:28 -0300

    endereçando html e caminhos

--- recipes/views.py
+++ recipes/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'victor'
+    })
 
-
-def sobre(request):
-    return HttpResponse("sobre")
-
-
-def contato(request):
-    return HttpResponse("contato")
