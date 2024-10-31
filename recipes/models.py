3a6b83245a7f32017d8e58758ee2dcb713a7cef1
victor <victor.em.1789@gmail.com>
2024-10-31 18:18:13 -0300

    adicionando os models ao admin

--- recipes/models.py
+++ recipes/models.py
@@ -17,7 +17,7 @@ class Recipe(models.Model):
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
-    preparation_steps = models.BooleanField(default = False)
+    preparation_steps_is_html = models.BooleanField(default = False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
@@ -29,3 +29,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True 
     )
+    
+    def __str__(self):
+        return self.title
+    
